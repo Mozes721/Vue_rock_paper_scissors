2de431d28dbfcbddcd19647f611fbed437cea0fd
Mozes721 <Richard.Taujenis@gmail.com>
2021-06-13 12:41:50 +0200

    scoreboard working with press

--- rock_paper_scissors/src/App.vue
+++ rock_paper_scissors/src/App.vue
@@ -1,22 +1,63 @@
 <template>
 <div id="app">
+      <!-- Header -->
       <div class="header-container">
         <h1>Rock Paper Scissors</h1>
       </div>
-      <Scoreboard/>
-      <Hands/>
+      <!-- ScoreBoard -->
+      <div class="scoreboard">
+    <div class="scorecontainer">
+      <h2 class="score_text">ScoreBoard</h2>
+      <div class="float-child">
+        <div class="player">
+          <h3>You</h3>
+          <h3 class="score">{{player_score}}</h3>
+        </div>
+      </div>
+      <div class="float-child">
+        <div class="oponent">
+          <h3>Opponent</h3>
+          <h3 class="score">{{opponent_score}}</h3>
+        </div>
+      </div>
+    </div>
   </div>
+      <!-- Game -->
+    <div class="float-child2">
+      <Player v-on:changeScore="updatePlayerScore($event)" />
+    </div>
+    <div class="float-child2">
+      <Opponent v-on:changeScore="updateOpponentScore($event)" />
+    </div>
+  </div>
+  
 </template>
 
 <script>
-import Scoreboard from './components/Scoreboard.vue'
-import Hands from './components/Hands.vue'
+import Player from './components/Player.vue'
+import Opponent from './components/Opponent.vue'
 export default {
   name: 'App',
+  data() {
+    return {
+      title: "Counter",
+      player_score: 0,
+      opponent_score: 0,
+    };
+  },
+
   components: {
-    Scoreboard,
-    Hands,
-  }
+    Player,
+    Opponent,
+  },
+  methods: {
+    updatePlayerScore(score){
+      this.player_score=score
+  },
+  updateOpponentScore(score){
+      this.opponent_score=score
+  },
+}
 }
 </script>
 
@@ -46,12 +87,59 @@ export default {
   text-align: center;
   font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
 }
-/* .header-container {
-  height: 15vh;
-  width: 100%;
-  background-color: #fff;
-} */
 
+.scoreboard {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+}
 
+.scorecontainer {
+  margin-top: 20px;
+  padding: 20px;
+  width: 25%;
+  height: 200px;
+  background-color:aliceblue;
+  border: 3px solid rgb(68, 59, 151);
+  border-radius: 10px;
+}
+.score_text {
+  background-color: aliceblue;
+  text-align: center;
+  font-family: 'Roboto Condensed', sans-serif;
+  border-bottom: 2px solid #000;
+  width:100%;
+}
+
+.float-child {
+  background-color:aliceblue;
+  width: 50%;
+  float: left;
+  padding: 20px;
+  
+}
+
+.float-child2 {
+  background-color:#292c34;
+  width: 50%;
+  float: left;
+  padding: 20px;
+}
+h3 {
+  background-color: aliceblue;
+  text-align: center;
+  font-family: 'Do Hyeon', sans-serif;
+  font-family: 'Roboto Condensed', sans-serif;
+  font-family: 'Source Code Pro', monospace;
+}
+.score {
+  padding: 1rem;
+  font-size: 40px;
+}
+
+.hands {
+  padding: 20px;
+}
 
 </style>
---
+++ rock_paper_scissors/src/components/Player.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+	<button class="button" @click="playerCounter">Press Me!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    playerCounter() {
+      this.counter++;
+      this.$emit('changeScore', this.counter)
+    },
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
